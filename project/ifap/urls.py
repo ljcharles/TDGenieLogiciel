67796cab5a6a30cf7bacdf491543133c76d2caee
freshloic <loicjeanchrles@gmail.com>
2018-09-26 23:42:09 +0200

    ajout d'une premiere vue ets des urls pour l'application ifap

--- project/ifap/urls.py
+++ project/ifap/urls.py
@@ -1,4 +1,4 @@
-"""project URL Configuration
+"""ifap URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
 
-from . import views
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('ifap/', include('ifap.urls')),
+    path('admin/', admin.site.urls),
 ]
