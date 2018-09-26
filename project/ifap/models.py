2abddfa1396aeca217a890ed0662203faf56d99e
freshloic <loicjeanchrles@gmail.com>
2018-09-27 00:02:20 +0200

    Ajout de ifap dans les settings et création des models

--- project/ifap/models.py
+++ project/ifap/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Classe(models.Model):
+    """docstring for Classe."""
+    nom = models.CharField(max_length=200)
+
+class Ordre(models.Model):
+    """docstring for Ordre."""
+    nom = models.CharField(max_length=200)
+    classe = models.ForeignKey(Classe,models.CASCADE)
+
+class Famille(models.Model):
+    """docstring for Famille."""
+    nom = models.CharField(max_length=200)
+    ordre = models.ForeignKey(Ordre,models.CASCADE)
+
+class Animal(models.Model):
+    """docstring for Animal."""
+    nomVernaculaire = models.CharField(max_length=200)
+    nomScientifique = models.CharField(max_length=200)
+    famille = models.ForeignKey(Famille,models.CASCADE)
