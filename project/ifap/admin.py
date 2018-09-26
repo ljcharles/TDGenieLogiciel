0fb14f3e8e611e29e1f47e53a37d64c6905ff781
freshloic <loicjeanchrles@gmail.com>
2018-09-27 00:24:16 +0200

    Modification du fichier admin.py pour la gestion des tables créées.

--- project/ifap/admin.py
+++ project/ifap/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Classe, Ordre, Famille, Animal
 
-# Register your models here.
+admin.site.register(Classe)
+admin.site.register(Ordre)
+admin.site.register(Famille)
+admin.site.register(Animal)
