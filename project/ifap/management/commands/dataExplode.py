21b9cccb9c883ab9adbe8f26253e58a8822dff9f
freshloic <loicjeanchrles@gmail.com>
2018-10-04 04:57:45 +0200

    changement du lien vers le fichier csv en absolu

--- project/ifap/management/commands/dataExplode.py
+++ project/ifap/management/commands/dataExplode.py
@@ -3,11 +3,16 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def handle(self, **options):
         from ifap.models import Classe, Ordre, Famille, Animal
+        from os.path import dirname, realpath
         import pandas as pd
-        data = pd.read_csv("/home/freshloic/Documents/UA/TDGenieLogiciel/project/ifap/management/commands/taxo.csv").to_dict(orient="row")
+
+        data = pd.read_csv( dirname(realpath(__file__)) + "/taxo.csv").to_dict(orient="row")
         # print(data)
 
+        print('Effacement des données prélables...')
         Classe.objects.all().delete()
+        print('Lancement des festivités...')
+        print("Ajout des données à la base ! Prenez un café c'est un peu long...")
 
         for item in data:
             # print(item['FAMILLE'])
@@ -15,3 +20,5 @@ class Command(BaseCommand):
             ordre = Ordre.objects.get_or_create(nom=item['ORDRE'], classe=classe)[0]
             famille = Famille.objects.get_or_create(nom=item['FAMILLE'], ordre=ordre)[0]
             Animal.objects.get_or_create(nomVernaculaire=item['NOM COMMUN (vernaculaire) de l\'espèce'], nomScientifique=item['NOM SCIENTIFIQUE de l\'espèce'], famille=famille)
+
+        print("C'est terminé !")
